c7791c3bd7fc1951b36d0f22be52a593ef97a87a
minnulekha <minnulekhavg@gmail.com>
2025-08-14 23:34:45 +0530

    URL error cleared

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000','https://blogit-frontend.vercel.app', credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://blogit-frontend.vercel.app'],
+  credentials: true
+}));
 
 // Serve uploaded images
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
